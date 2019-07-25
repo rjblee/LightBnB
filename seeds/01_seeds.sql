f0d65e476d90348146436ba655672220ead8fbdc
Ralph <ralph.lee@live.ca>
2019-07-25 00:51:40 +0000

    Added data into tables

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,19 @@
+INSERT INTO users (name, email, password)
+  VALUES ('Ralph Lee', 'ralph.lee@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+         ('Grayson Lee', 'grayson.lee@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+         ('Scott Appleton', 'scott.appleton@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+  
+
+
+-- INSERT INTO properties 
+
+
+
+-- INSERT INTO reservations
+
+
+
+-- INSERT INTO property_reviews
+
+
+
