a654f2cd692ffce3ea6a392ba33e0408fc54525b
Ralph <ralph.lee@live.ca>
2019-07-25 23:18:56 +0000

    Updated getAllReservations function

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -100,6 +100,18 @@ exports.addUser = addUser;
  */
 const getAllReservations = function(guest_id, limit = 10) {
 
+  return pool.query(`
+  SELECT reservations.*, properties.*, avg(rating) as average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON property_reviews.property_id = properties.id
+  WHERE reservations.guest_id = 1 AND reservations.end_date < now()::date
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date DESC
+  LIMIT $1;
+  `, [limit])
+  .then(res => res.rows);
+
   // return getAllProperties(null, 2);
 }
 exports.getAllReservations = getAllReservations;
